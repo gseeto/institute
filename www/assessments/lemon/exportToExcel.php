3e937d0a297d29297f97d9bb8230c6d5d73b966a
Gareth <gseeto@gseeto-Ubuntu-VirtualBox.(none)>
2014-11-10 16:13:35 -0800

    adding email to lemon export

--- www/assessments/lemon/exportToExcel.php
+++ www/assessments/lemon/exportToExcel.php
@@ -13,14 +13,14 @@ function EscapeCsv($strString) {
 
 	header('Content-Type: text/csv');
 	header('Content-Disposition: attachment; filename=exportLemon.csv');
-	print "Name,Luminary,Entrepreneur,Manager,Organizer,Networker\r\n";
+	print "Name,Email,Luminary,Entrepreneur,Manager,Organizer,Networker\r\n";
 	
 	
 	$strArgs = substr(QApplication::$PathInfo, 1 );
 	$assessmentArray = explode('/',$strArgs);
 	foreach($assessmentArray as $intAssessmentId) {
 		$objAssessment = LemonAssessment::Load($intAssessmentId);
-		printf("%s %s, %s, %s, %s, %s, %s \r\n",
-		$objAssessment->User->FirstName, $objAssessment->User->LastName, $objAssessment->L,$objAssessment->E,$objAssessment->M,$objAssessment->O,$objAssessment->N);		
+		printf("%s %s,%s, %s, %s, %s, %s, %s \r\n",
+		$objAssessment->User->FirstName, $objAssessment->User->LastName, $objAssessment->User->Email, $objAssessment->L,$objAssessment->E,$objAssessment->M,$objAssessment->O,$objAssessment->N);		
 	}
 ?>
