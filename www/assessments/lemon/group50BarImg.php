71cfe9368cf0b56d430d319bc497d8c3aeeb38d3
Gareth <gseeto@gseeto-Ubuntu-VirtualBox.(none)>
2017-06-01 12:39:14 -0700

    www/assessments/groupAggregation.php www/assessments/groupAggregation.tpl.php Adding group assessment ability for lemon 50 questions assessments.

---
+++ www/assessments/lemon/group50BarImg.php
@@ -0,0 +1,96 @@
+<?php // content="text/plain; charset=utf-8"
+require(dirname(__FILE__) . '/../../../includes/prepend.inc.php');
+require_once (dirname(__FILE__) . '/../../../includes/jpgraph/jpgraph.php');
+require_once (dirname(__FILE__) . '/../../../includes/jpgraph/jpgraph_bar.php');
+require_once (dirname(__FILE__) . '/../../../includes/jpgraph/jpgraph_table.php');
+
+// We need some data
+$labelArray = array();
+$lemonValues = array(0,0,0,0,0);
+
+for($i=1; $i<6; $i++) {
+	$labelArray[] = LemonType::ToString($i);
+}
+
+$total = 0;
+$strArgs = substr(QApplication::$PathInfo, 1 );
+$assessmentArray = explode('/',$strArgs);
+foreach($assessmentArray as $intAssessmentId) {
+	$objAssessment = Lemon50Assessment::Load($intAssessmentId);
+	if($objAssessment->L) {
+		$lemonValues[0] += $objAssessment->L;
+		$lemonValues[1] += $objAssessment->E;
+		$lemonValues[2] += $objAssessment->M;
+		$lemonValues[3] += $objAssessment->O;
+		$lemonValues[4] += $objAssessment->N;
+		$total+= $objAssessment->L + $objAssessment->E + $objAssessment->M + $objAssessment->O + $objAssessment->N;
+	} else {
+		$resultArray = Lemon50AssessmentResults::LoadArrayByAssessmentId($intAssessmentId);
+		foreach($resultArray as $objResult) {
+			$intIndex = $objResult->Question->LemonTypeId - 1;
+			$lemonValues[$intIndex] += $objResult->Value;
+			$total+= $objResult->Value;
+		}
+	}
+}
+
+// Get percentages of all the slices - or lets get the average for each type instead
+for($i=0; $i<5; $i++) {
+	$lemonValues[$i] = ($lemonValues[$i]/$total)*100;
+	//$lemonValues[$i] = ($lemonValues[$i]/count($assessmentArray));
+}
+
+
+$datay = array($labelArray,$lemonValues);
+
+// Some basic defines to specify the shape of the bar+table
+$nbrbar = 5;
+$cellwidth = 110;
+$tableypos = 300;
+$tablexpos = 60;
+$tablewidth = $nbrbar*$cellwidth;
+$rightmargin = 30;
+
+// Overall graph size
+$height = 350;
+$width = $tablexpos+$tablewidth+$rightmargin;
+
+// Create the basic graph. 
+$graph = new Graph($width,$height);	
+$graph->img->SetMargin($tablexpos,$rightmargin,30,$height-$tableypos);
+$graph->SetScale("textlin");
+$graph->SetMarginColor('white');
+
+// Setup titles and fonts
+$graph->title->Set('LEMON Results');
+$graph->SetUserFont('dejavu/DejaVuSans.ttf');
+$graph->title->SetFont(FF_USERFONT,FS_NORMAL,18);
+$graph->xaxis->HideLabels();
+
+$bplot = new BarPlot($datay[1]);
+$bplot->SetFillColor(array('#6C98B0','#8AC16E','#FFD43F','#FCA343','#E66E24'));
+$bplot->SetColor("black");
+$graph->graph_theme = null;
+$graph->Add($bplot);
+
+//Setup the table
+$table = new GTextTable();
+$table->Set($datay);
+$table->SetPos($tablexpos,$tableypos+1);
+
+// Basic table formatting
+$table->SetFont(FF_USERFONT,FS_NORMAL,8);
+$table->SetAlign('center');
+$table->SetMinColWidth($cellwidth);
+$table->SetNumberFormat('%0.2f');
+
+// Format table header row
+$table->SetRowFillColor(0,'teal@0.7');
+$table->SetRowFont(0,FF_USERFONT,FS_NORMAL,9);
+$table->SetRowAlign(0,'center');
+
+// .. and add it to the graph
+$graph->Add($table);
+$graph->Stroke();
+
+?>
---
+++ www/assessments/lemon/group50FirstSecondImg.php
@@ -0,0 +1,139 @@
+<?php // content="text/plain; charset=utf-8"
+require(dirname(__FILE__) . '/../../../includes/prepend.inc.php');
+require_once (dirname(__FILE__) . '/../../../includes/jpgraph/jpgraph.php');
+require_once (dirname(__FILE__) . '/../../../includes/jpgraph/jpgraph_bar.php');
+
+setlocale (LC_ALL, 'et_EE.ISO-8859-1');
+$labels = array();
+for($i=1; $i<6; $i++) {
+	$labels[] = LemonType::ToString($i);
+}
+$data1y=array(0,0,0,0,0);
+$data2y=array(0,0,0,0,0);
+$strArgs = substr(QApplication::$PathInfo, 1 );
+$assessmentArray = explode('/',$strArgs);
+foreach($assessmentArray as $intAssessmentId) {
+	$lemonValues = array();
+	$objAssessment = Lemon50Assessment::Load($intAssessmentId);
+	if($objAssessment->L) {
+		$lemonValues['Luminary'] = $objAssessment->L;
+		$lemonValues['Entrepreneur'] = $objAssessment->E;
+		$lemonValues['Manager'] = $objAssessment->M;
+		$lemonValues['Organizer'] = $objAssessment->O;
+		$lemonValues['Networker'] = $objAssessment->N;
+	} else {
+		$resultArray = Lemon50AssessmentResults::LoadArrayByAssessmentId($intAssessmentId);
+		foreach($resultArray as $objResult) {
+			$intLemonType = $objResult->Question->LemonTypeId;
+			switch($intLemonType) {
+				case 1:
+					$key = 'Luminary';
+					break;
+				case 2:
+					$key = 'Entrepreneur';
+					break;
+				case 3:
+					$key = 'Manager';
+					break;
+				case 4:
+					$key = 'Organizer';
+					break;
+				case 5:
+					$key = 'Networker';
+					break;
+			}
+			if(array_key_exists($key,$lemonValues)) {
+				$lemonValues[$key] += $objResult->Value;
+			} else {
+				$lemonValues[$key] = $objResult->Value;
+			}
+		}
+	}
+	$i=1;
+	arsort($lemonValues,SORT_NUMERIC);	
+	foreach($lemonValues as $key=>$value) {
+		if(($i==1) ||($i==2)) {
+			switch($key) {
+				case 'Luminary':
+					if($i==1) 
+						$data1y[0] += 1;
+					else 
+						$data2y[0] += 1;
+					break;
+				case 'Entrepreneur':
+					if($i==1) 
+						$data1y[1] += 1;
+					else 
+						$data2y[1] += 1;
+					break;
+				case 'Manager':
+					if($i==1) 
+						$data1y[2] += 1;
+					else 
+						$data2y[2] += 1;
+					break;
+				case 'Organizer':
+					if($i==1) 
+						$data1y[3] += 1;
+					else 
+						$data2y[3] += 1;
+					break;
+				case 'Networker':
+					if($i==1) 
+						$data1y[4] += 1;
+					else 
+						$data2y[4] += 1;
+					break;
+			}	
+		}
+		$i++;
+	}
+	
+}
+
+// Create the graph. These two calls are always required
+$graph = new Graph(610,400);	
+$graph->SetScale("textlin");
+$graph->yaxis->scale->ticks->Set(1.0);
+
+$graph->SetShadow();
+$graph->img->SetMargin(80,30,40,80);
+
+// Create the bar plots
+// Primary
+$b1plot = new BarPlot($data1y);
+$b1plot->SetShadow();
+$b1plot->SetFillColor(array('#6C98B0','#8AC16E','#FFD43F','#FCA343','#E66E24'));
+
+//Secondary
+$b1plot->SetColor('#2C7CBA');
+$b2plot = new BarPlot($data2y);
+$b2plot->SetShadow();
+$b2plot->SetNoFill();
+
+$b2plot->SetColor('#93C0E2');
+$b2plot->SetFillColor(array('#6C98B0@0.6','#8AC16E@0.6','#FFD43F@0.6','#FCA343@0.6','#E66E24@0.6'));
+
+
+
+// Create the grouped bar plot
+$gbplot = new AccBarPlot(array($b1plot,$b2plot));
+
+// ...and add it to the graPH
+$graph->graph_theme = null;
+$graph->Add($gbplot);
+
+$graph->title->Set("First And Second Slices");
+$graph->SetUserFont('dejavu/DejaVuSans.ttf');
+$graph->title->SetFont(FF_USERFONT,FS_NORMAL,18);
+
+//$graph->xaxis->title->Set("Spheres Or Sectors");
+$graph->xaxis->SetTickLabels($labels);
+$graph->xaxis->SetLabelAngle(45); // 45 degrees angle
+
+$graph->yaxis->title->SetFont(FF_USERFONT,FS_NORMAL);
+$graph->xaxis->title->SetFont(FF_USERFONT,FS_NORMAL);
+
+// Display the graph
+$graph->Stroke();
+?>
